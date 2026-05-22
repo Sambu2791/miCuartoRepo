d214cd7520a125d1dfb1f3d9d2606eb380a3d9e3
Sambu <amsam27@gmail.com>
2026-05-22 16:32:28 -0500

    adicion elementos platzi

--- api/app.py
+++ api/app.py
@@ -21,5 +21,5 @@ def get_warriors_got():
     rows = ["Jon Snow", "Daenerys Targaryen", "Arya Stark", "Tyrion Lannister", "Cersei Lannister", "Jaime Lannister", "Bran Stark", "Sansa Stark"]
     return rows
 
-@app.get("/cursosPlatzi")def get_cursos():    cursos = ["Docker", "Bash", "Linux", "Inglés","Python" , "Javascript", "Azure", "DevOps"]    return cursos
-```@app.get("/cursosPlatzi")def get\_cursos(): cursos *=* \["Docker", "Bash", "Linux", "Inglés","Python" , "Javascript", "Azure", "DevOps"] *return* cursos
+@app.get("/cursosPlatzi")def get\_cursos(): cursos *=* \["Docker", "Bash", "Linux", "Inglés","Python" , "Javascript", "Azure", "DevOps"] *return* cursos
+
